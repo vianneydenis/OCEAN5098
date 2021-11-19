b7471acd86c429f50fbd73a138fd09f4aea38f76
Vianney <vianney.denis@gmail.com>
2021-11-19 17:34:09 +0800

    Commit Loop 2021/11/19

---
+++ Log_growth.R
@@ -0,0 +1,26 @@
+# Solution below
+grow <- function (growth.rate, num_gen) {
+  N <- rep (0,num_gen)
+  generation=rep(0,num_gen)
+  generation[1]=1
+  N[1] <- 10
+  for (i in 2:num_gen) {
+    N[i]= N[i-1]+(growth.rate * N[i-1] * ((100-
+                                             N[i-1])/100))
+    generation [i]=i
+  }
+  plot(N~generation, type='l',lwd=3, ylim=c(1,150))
+}
+grow (0.1,50)
+grow (0.5,50)
+grow (1.3,50)
+grow (2.52,50)
+grow (3,50)
+
+#
+saveGIF({
+  a<-seq(0.1,3.0, 0.05)
+  for (i in 1:length(a)){
+    grow(a[i],50)
+  }
+}, interval = 0.1, ani.width = 950, ani.height = 550)
