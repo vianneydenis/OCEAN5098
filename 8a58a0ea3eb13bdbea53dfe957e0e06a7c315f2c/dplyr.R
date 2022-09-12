e7279b0dcfb460e38feecb942d9c072e7a6e834a
Vianney DENIS <vianney.denis@gmail.com>
2022-09-12 16:03:06 +0800

    Update mac 20220912

--- 8a58a0ea3eb13bdbea53dfe957e0e06a7c315f2c/dplyr.R
+++ 8a58a0ea3eb13bdbea53dfe957e0e06a7c315f2c/dplyr.R
@@ -4,40 +4,40 @@ summarised <- summarise(iris, Mean.Width = mean(Sepal.Width))
 head(summarised)
 
 # by column names
-selected <- dplyr::select(iris, Sepal.Length, Sepal.Width, Petal.Length)
-head(selected) 
+selected1 <- dplyr::select(iris, Sepal.Length, Sepal.Width, Petal.Length)
+head(selected1) 
 
 # by column range
-selected1 <- dplyr::select(iris, Sepal.Length:Petal.Length)
-head(selected1, 4)
+selected2 <- dplyr::select(iris, Sepal.Length:Petal.Length)
+head(selected2, 4)
 
 # by column range number
-selected2 <- dplyr::select(iris,c(2:5))
-head(selected2)
+selected3 <- dplyr::select(iris,c(2:5))
+head(selected3)
 
-selected <- select(iris, -Sepal.Length, -Sepal.Width)
+selected4 <- dplyr::select(iris, -Sepal.Length, -Sepal.Width)
 
 # Select setosa species
-filtered <- filter(iris, Species == "setosa" )
-head(filtered,3)
+filtered1 <- filter(iris, Species == "setosa" )
+head(filtered1,3)
 
 # Select versicolor species with Sepal width more than 3
-filtered1 <- filter(iris, Species == "versicolor", Sepal.Width > 3)
-tail(filtered1)
+filtered2 <- filter(iris, Species == "versicolor", Sepal.Width > 3)
+tail(filtered2)
 
-#To create a column “Greater.Half” which stores TRUE if given condition is TRUE
+#To create a column “Greater.Half” which stores a logical vector
 mutated1 <- mutate(iris, Greater.Half = Sepal.Width > 0.5 * Sepal.Length)
 tail(mutated1)
 
 table(mutated1$Greater.Half)
 
 # Sepal Width by ascending order
-arranged <- arrange(iris, Sepal.Width)
-head(arranged)
+arranged1 <- arrange(iris, Sepal.Width)
+head(arranged1)
 
 # Sepal Width by descending order
-arranged1 <- arrange(iris, desc(Sepal.Width))
-head(arranged1)
+arranged2 <- arrange(iris, desc(Sepal.Width))
+head(arranged2)
 
 # Mean sepal width by Species
 gp <- group_by(iris, Species)
