f479e65e1f3a6bebeb8f29f92c8b2aaa2c4afb91
Vianney <vianney.denis@gmail.com>
2022-11-05 15:13:06 +0800

    20221105_linear

--- 8a58a0ea3eb13bdbea53dfe957e0e06a7c315f2c/linear.R
+++ 8a58a0ea3eb13bdbea53dfe957e0e06a7c315f2c/linear.R
@@ -1,3 +1,13 @@
+xaringanExtra::use_clipboard()
+
+
+
+
+
+
+
+
+
 library(Hmisc)
 library(corrplot)
 library(MASS)
@@ -9,7 +19,9 @@ library(readr)
 library(lme4)
 library (lmerTest)
 library(nlme)
+library(gvlma)
 
+# correlation
 rairuoho<-read.table('https://www.dipintothereef.com/uploads/3/7/3/5/37359245/rairuoho.txt',header=T, sep="\t", dec=".")
 cor.test(rairuoho$day6, rairuoho$day7)
 
@@ -27,10 +39,10 @@ abline(lm(rairuoho$day7~rairuoho$day6), col="red", lwd=2)
 #  stat_smooth(method = "lm", col = "red")
 
 model1 <- lm(Petal.Width ~ Petal.Length, data = iris)
-model1
+model1$coefficients
 
 ggplot(iris, aes(x = Petal.Length, y = Petal.Width)) +
-  geom_point() +
+  geom_point(aes(fill=Species),shape = 21, size=5) +
   stat_smooth(method = "lm", col = "blue")
 
 summary(model1)
@@ -39,198 +51,68 @@ confint(model1)
 
 sigma(model1)*100/mean(iris$Petal.Width)
 
-model2 <- lm(Petal.Width ~ Petal.Length + Sepal.Width + Sepal.Length, data = iris)
-summary(model2)
+fit1 <- lm(Petal.Width ~ Petal.Length + Sepal.Width + Sepal.Length, data = iris)
+summary(fit1)
 
-summary(model2)$coefficient
+# Other useful functions
+coefficients(fit1) # model coefficients
+confint(fit1, level=0.95) # CIs for model parameters
+fitted(fit1) # predicted values
+residuals(fit1) # residuals
+anova(fit1) # anova table
+vcov(fit1) # covariance matrix for model parameters
+influence(fit1) # regression diagnostics
 
-confint(model2)
+# diagnostic plots
+layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
+plot(fit1)
 
-sigma(model2)/mean(iris$Petal.Width)
+# Assessing Outliers
+outlierTest(fit1) # Bonferonni p-value for most extreme obs
+qqPlot(fit1, main="QQ Plot") #qq plot for studentized resid
+leveragePlots(fit1) # leverage plots
 
-model3 <- lm(Petal.Width ~., data = iris[,1:4])
-# the "." means: take all variable except as predictors exept Petal.Width which is our response variable
+# Influential Observations
+# added variable plots
+avPlots(fit1)
+# Cook's D plot
+# identify D values > 4/(n-k-1)
+cutoff <- 4/((nrow(iris)-length(fit1$coefficients)-2))
+plot(fit1, which=4, cook.levels=cutoff)
+# Influence Plot
+influencePlot(fit1, id.method="identify", main="Influence Plot", sub="Circle size is proportial to Cook's Distance" )
 
-model4 <- lm(Petal.Width ~. -Sepal.Width, data = iris[,1:4])
+# Normality of Residuals
+# qq plot for studentized resid
+qqPlot(fit1, main="QQ Plot")
+# distribution of studentized residuals
+sresid <- studres(fit1)
+hist(sresid, freq=FALSE,
+   main="Distribution of Studentized Residuals")
+xfit<-seq(min(sresid),max(sresid),length=40)
+yfit<-dnorm(xfit)
+lines(xfit, yfit)
 
-model5 <-  update(model2,  ~. -Sepal.Length)
+# Evaluate homoscedasticity
+# non-constant error variance test
+ncvTest(fit1)
+# plot studentized residuals vs. fitted values
+spreadLevelPlot(fit1)
 
-BIC(model3); BIC(model4);BIC(model5)
-model3 <- lm(Petal.Width ~., data = iris[,1:4])
-AIC(model3); AIC(model4);AIC(model5)
+# Evaluate Collinearity
+vif(fit1) # variance inflation factors
+sqrt(vif(fit1)) > 2 # problem?
 
-#Stepwise Selection based on AIC
-step <- stepAIC(model3, direction='backward')
-summary(step)
+# Evaluate Nonlinearity
+# component + residual plot
+crPlots(fit1)
+# Ceres plots
+ceresPlots(fit1)
 
-#Stepwise Selection with BIC
-n = dim(iris[,1:4])[1]
-stepBIC = stepAIC(model3,k=log(n), direction='backward')
-summary(stepBIC)
+# Test for Autocorrelated Errors
+durbinWatsonTest(fit1)
 
-model3 <- lm(Petal.Width ~., data = iris[,1:4])
-
-plot(model3)
-
-plot(model3,1)
-
-plot(model3,2)
-
-plot(model3,3)
-
-plot(model3,5)
-
-corr<-cor(iris[,1:3])
-p.val<-rcorr(as.matrix(rairuoho[,1:3]))$P
-corrplot(corr,type='upper',method='color', addCoef.col = "black", p.mat=as.matrix(p.val), sig.level = 0.05,diag=F)
-
-vif(model3)
-#remove Petal.Length
-model6 <- lm(Petal.Width ~. -Petal.Length, data = iris[,1:4])
-vif(model6)
-plot(model6)
-
-iris.lm<-lm(Petal.Width ~ Species, data=iris)
-summary(iris.lm)
-anova(iris.lm)
-
-pirateplot(formula = time ~ cleaner + type,
-           data = poopdeck,
-           ylim = c(0, 150),
-           xlab = "Cleaner",
-           ylab = "Cleaning Time (minutes)",
-           main = "poopdeck data",
-           back.col = gray(.97), 
-           cap.beans = TRUE, 
-           theme = 2)
-
-# Step 1: Create an aov object
-mod.aov <- aov(formula = y ~ x1 + x2 + ..., data = data)
-
-# Step 2: Look at a summary of the aov object
-summary(mod.aov)
-
-# Step 3: Calculate post-hoc tests
-TukeyHSD(mod.aov)
-
-# Step 4: Look at coefficients
-mod.lm <- lm(formula = y ~ x1 + x2 + ..., data = data)
-summary(mod.lm)
-
-pirateplot(time ~ cleaner, 
-                  data = poopdeck, 
-                  theme = 2, 
-                  cap.beans = TRUE,
-                  main = "formula = time ~ cleaner")
-
-# Step 1: aov object with time as DV and cleaner as IV
-cleaner.aov <- aov(formula = time ~ cleaner, data = poopdeck)
-
-# Step 2: Look at the summary of the anova object
-summary(cleaner.aov)
-
-# Step 3: Conduct post-hoc tests
-TukeyHSD(cleaner.aov)
-
-# Step 4: Create a regression object
-cleaner.lm <- lm(formula = time ~ cleaner, data = poopdeck)
-summary(cleaner.lm)
-
-# Step 1: aov object with time as DV and cleaner and type as IV
-cleaner.type.aov <- aov(formula = time ~ cleaner + type, data = poopdeck)
-
-# Step 2: Get ANOVA table with summary()
-summary(cleaner.type.aov)
-
-# Step 3: Conduct post-hoc tests
-TukeyHSD(cleaner.type.aov)
-
-# Step 4: Look at regression coefficients
-cleaner.type.lm <- lm(formula = time ~ cleaner + type, data = poopdeck)
-summary(cleaner.type.lm)
-
-# Step 1: Create ANOVA object with interactions
-cleaner.type.int.aov <- aov(formula = time ~ cleaner * type, data = poopdeck)
-# Step 2: Look at summary table
-summary(cleaner.type.int.aov)
-
-# Step 4: Calculate regression coefficients
-cleaner.type.int.lm <- lm(formula = time ~ cleaner * type, data = poopdeck)
-summary(cleaner.type.int.lm)
-
-cat_plot(cleaner.type.int.lm, pred = cleaner, modx = type, interval = TRUE)
-
-fitiris <- lm(Petal.Width ~ Petal.Length*Species, data = iris)
-interact_plot(fitiris, pred = Petal.Length, modx = Species, plot.points=T)
-
-fitiris <- lm(Petal.Width ~ Petal.Length*Species, data = iris)
-# In evaluating an ANCOVA model, we want to sequentially ask first whether there is a difference in slope, then if there is not, look for differences in intercept. 
-# In addition, there is variety of way to calculate the sum of squares. Here (but this debatable), we would prefer a type 3 Anova especially since we have an interaction term in our Ancova (see Homogeneity of slope assumption in ANCOVA)
-car::Anova(fitiris, type = 3)
-
-# Step 1: Calculate regression object with lm()
-time.lm <- lm(formula = time ~ type + cleaner, data = poopdeck)
-
-# Type I ANOVA - aov()
-time.I.aov <- aov(time.lm)
-# or anova(time.lm)
-
-# Type II ANOVA - Anova(type = 2)
-time.II.aov <- car::Anova(time.lm, type = 2)
-
-# Type III ANOVA - Anova(type = 3)
-time.III.aov <- car::Anova(time.lm, type = 3)
-
-# Are observations in the poopdeck data balanced?
-with(poopdeck, table(cleaner, type)) 
-
-names(cleaner.type.int.aov)
-
-poopdeck$int.fit <- cleaner.type.int.aov$fitted.values
-poopdeck$me.fit <- cleaner.type.aov$fitted.values
-
-mean(abs(poopdeck$int.fit - poopdeck$time))
-mean(abs(poopdeck$me.fit - poopdeck$time))
-
-shapiro.test(cleaner.type.int.aov$residuals) # test our residuals vs a normal distribution
-bartlett.test(cleaner.type.int.aov$residuals ~ interaction(cleaner, type), data = poopdeck) # test variance of our residuals in the different groups.
-
-shag <- read.csv("Data/shagLPI.csv", header = TRUE)
-shag$year <- as.numeric(shag$year)  #  year should be a numeric variable
-shag.hist <- ggplot(shag, aes(pop)) + geom_histogram() 
-shag.hist
-
-shag.m <- glm(pop ~ year, family = poisson, data = shag)
-summary(shag.m)
-
-shag.p <- ggplot(shag, aes(x = year, y = pop)) +
-    geom_point(colour = "#483D8B") +
-    geom_smooth(method = "glm", method.args = list(family = 'poisson'),se=TRUE, colour = "#483D8B", fill = "#483D8B", alpha = 0.6) +
-    scale_x_continuous(breaks = c(1975, 1980, 1985, 1990, 1995, 2000, 2005)) +
-    labs(x = " ", y = "European Shag abundance")
-shag.p
-
-Weevil_damage <- read.csv("Data/Weevil_damage.csv")
-Weevil_damage$block <- as.factor(Weevil_damage$block) # Making block a factor
-weevil.m <- glm(damage_T_F ~ block, family = binomial, data = Weevil_damage)
-summary(weevil.m)
-
-rairuoho$ID<-rownames(rairuoho)
-rai<-rairuoho %>% pivot_longer(day3:day8, names_to = "day", values_to = "length")
-rai$day<-parse_number(rai$day)
-rai
-
-lmer.rai<-lmer(length ~ day + (1|ID), data=rai, REML=TRUE) # REML default
-summary(lmer.rai)
-
-anova(lmer.rai)
-
-rand(lmer.rai)
-
-lme.rai = lme(length ~ day, random=~1|ID, data=rai)
-summary(lme.rai)
-
-lm.rai<-lm(length ~ day, data= rai)
-anova (lme.rai, lm.rai)
+gvmodel <- gvlma(fit1)
+summary(gvmodel)
 ```{.r .distill-force-highlighting-css}
 ```
